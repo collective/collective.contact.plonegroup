e85efab4ae2e28bccc90664e80d53c6bdf7a9d52
Stephan Geulette <s.geulette@imio.be>
2013-03-21 14:44:21 +0100

    Added record modified event, creating group when changing organization list

--- src/collective/contact/plonegroup/browser/settings.py
+++ src/collective/contact/plonegroup/browser/settings.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from zope import schema
+from zope.component import getUtility
 from zope.interface import Interface
 from z3c.form import form
+from plone import api
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.autoform.directives import widget
 #from plone.formwidget.contenttree import PathSourceBinder
 #from plone.formwidget.contenttree import MultiContentTreeFieldWidget
+from plone.registry.interfaces import IRecordModifiedEvent, IRegistry
 from plone.z3cform import layout
 from collective.z3cform.datagridfield import DataGridFieldFactory
 from collective.z3cform.datagridfield.registry import DictRow
@@ -59,6 +62,45 @@ class IContactPlonegroupConfig(Interface):
                            schema=IFunctionSchema)
     )
     widget(functions=DataGridFieldFactory)
+
+
+def addOrModifyGroup(group_name, organization_title, function_title):
+    """
+        create a plone group
+    """
+    if isinstance(organization_title, unicode):
+        organization_title = organization_title.encode('utf8')
+    if isinstance(function_title, unicode):
+        function_title = function_title.encode('utf8')
+    group = api.group.get(groupname=group_name)
+    group_title = '%s (%s)' % (organization_title, function_title)
+    if group is None:
+        group = api.group.create(
+            groupname=group_name,
+            title=group_title,
+        )
+    else:
+        #group_title is maybe modified
+        if group.getProperty('title') != group_title:
+            group.setProperties(title=group_title)
+
+
+def detectContactPlonegroupChange(event):
+    """
+        Manage our record changes
+    """
+    if IRecordModifiedEvent.providedBy(event) and event.record.interface == IContactPlonegroupConfig:
+        if event.record.fieldName == 'organizations':
+            old_set = set([(dic['org_id'], dic['org_title']) for dic in event.oldValue])
+            new_set = set([(dic['org_id'], dic['org_title']) for dic in event.newValue])
+            # we detect a new organization
+            add_set = new_set.difference(old_set)
+            registry = getUtility(IRegistry)
+            for (new_id, new_title) in add_set:
+                for fct_dic in registry['collective.contact.plonegroup.browser.settings.'
+                                        'IContactPlonegroupConfig.functions']:
+                    group_name = "%s_%s" % (new_id, fct_dic['fct_id'])
+                    addOrModifyGroup(group_name, new_title, fct_dic['fct_title'])
 
 
 class SettingsEditForm(RegistryEditForm):
