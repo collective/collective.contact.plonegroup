b431068765694d271f4da6035ad25ee0263f0390
Gauthier Bastien <g.bastien@imio.be>
2020-09-30 18:21:15 +0200

    Use imio.helpers.testing_logger to see logger message in tests too

--- src/collective/contact/plonegroup/setuphandlers.py
+++ src/collective/contact/plonegroup/setuphandlers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from collective.contact.plonegroup import logger
 from config import FUNCTIONS_REGISTRY
 from config import ORGANIZATIONS_REGISTRY
+from imio.helpers.testing import testing_logger
 from plone.registry.interfaces import IRegistry
 from ZODB.POSException import ConnectionStateError
 from zope.component import getUtility
@@ -20,5 +20,6 @@ def postInstall(context):
         try:
             registry[FUNCTIONS_REGISTRY] = []
         except ConnectionStateError:
-            logger('!!!Failed to set registry functions to []!!!')
+            logger = testing_logger('collective.contact.plonegroup')
+            logger.warn('!!!Failed to set registry functions to []!!!')
             registry.records[FUNCTIONS_REGISTRY].field.value_type = None
