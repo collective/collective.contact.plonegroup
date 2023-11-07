454aebf8faca3d9d6670babd7910c46029499e13
Stephan Geulette <s.geulette@imio.be>
2023-11-07 11:55:06 +0100

    Used a default vocabulary that can be overrided

--- src/collective/contact/plonegroup/behaviors.py
+++ src/collective/contact/plonegroup/behaviors.py
@@ -9,21 +9,15 @@ from plone import api
 from plone.autoform import directives
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.formwidget.masterselect import MasterSelectField
+from plone.memoize import ram
 from plone.supermodel import model
 from zope import schema
 from zope.component import getUtility
 from zope.interface import alsoProvides
 from zope.interface import implementer
+from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
-
-
-def get_principal_suffixes():
-    return get_all_suffixes()
-
-
-def get_organizations_voc_name():
-    return 'collective.contact.plonegroup.browser.settings.SelectedOrganizationsElephantVocabulary'
 
 
 def filter_user_organizations(userid):
@@ -32,15 +26,12 @@ def filter_user_organizations(userid):
     """
     if not userid:
         return SimpleVocabulary([])
-    suffixes = get_principal_suffixes()
-    # groups = api.group.get_groups(user=user)
-    groups = get_plone_groups_for_user(user_id=userid)
-    user_orgs = organizations_with_suffixes(groups, suffixes, group_as_str=True)
-    factory = getUtility(IVocabularyFactory, get_organizations_voc_name())
-    vocab = factory(None)
-    return SimpleVocabulary([term for term in vocab._terms if term.value in user_orgs])
+    factory = getUtility(IVocabularyFactory, 'collective.contact.plonegroup.primary_organizations')
+    vocab = factory(None, userid=userid)
+    return SimpleVocabulary([term for term in vocab._terms])
 
 
+@provider(IFormFieldProvider)
 class IPlonegroupUserLink(model.Schema):
 
     userid = MasterSelectField(
@@ -62,10 +53,31 @@ class IPlonegroupUserLink(model.Schema):
     primary_organization = schema.Choice(
         title=_(u'Primary organization'),
         required=False,
-        vocabulary=get_organizations_voc_name(),
+        vocabulary='collective.contact.plonegroup.browser.settings.SelectedOrganizationsElephantVocabulary',
     )
     directives.read_permission(primary_organization='collective.contact.plonegroup.read_userlink_fields')
     directives.write_permission(primary_organization='collective.contact.plonegroup.write_userlink_fields')
 
 
-alsoProvides(IPlonegroupUserLink, IFormFieldProvider)
+def primary_organizations_vocabulary_cache_key(method, self, context, userid, suffixes, base_voc):
+    date = get_cachekey_volatile('_users_groups_value')
+    return date, userid, suffixes, base_voc
+
+
+@implementer(IVocabularyFactory)
+class PrimaryOrganizationsVocabulary(object):
+    """ Primary organizations vocabulary """
+
+    # @ram.cache(primary_organizations_vocabulary_cache_key)
+    def PrimaryOrganizationsVocabulary__call__(self, context, userid=None, suffixes=[],
+                                               base_voc='collective.contact.plonegroup.browser.settings.'
+                                                        'SelectedOrganizationsElephantVocabulary'):
+        if not suffixes:
+            suffixes = get_all_suffixes()
+        groups = get_plone_groups_for_user(user_id=userid)
+        user_orgs = organizations_with_suffixes(groups, suffixes, group_as_str=True)
+        factory = getUtility(IVocabularyFactory, base_voc)
+        vocab = factory(None)
+        return SimpleVocabulary([term for term in vocab._terms if term.value in user_orgs])
+
+    __call__ = PrimaryOrganizationsVocabulary__call__
